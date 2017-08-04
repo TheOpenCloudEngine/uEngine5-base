dcc849b53a0121ecc6584e23c22569686b184f42
SeungpilPark <sppark@uengine.org>
2017-08-04 11:36:03 +0900

    #1 Separate from mw4

--- src/main/java/org/uengine/social/service/worklist/WorkListRepository.java
+++ src/main/java/org/uengine/social/service/worklist/WorkListRepository.java
@@ -14,5 +14,7 @@ import java.util.List;
 public interface WorkListRepository extends PersistentRepository<WorkList, Long> {
 
     public List<WorkList> findByInstId(@Param("instId") Long instId);
+
+
 }
 
