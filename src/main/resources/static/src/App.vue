59fa01fc73403a267786ddd8c688d2a19297b8f2
jinyoung jang <jyjang@uengine.org>
2017-08-21 00:04:25 +0900

    no message

--- src/main/resources/static/src/App.vue
+++ src/main/resources/static/src/App.vue
@@ -79,7 +79,7 @@
         <v-container fluid>
           <router-view></router-view>
 
-          <form novalidate @submit.stop.prevent="submit">
+          <form novalidate @submit.stop.prevent="submit" v-if="false">
             <md-input-container>
               <label>새로운 주제</label>
               <md-input v-model="title"></md-input>
