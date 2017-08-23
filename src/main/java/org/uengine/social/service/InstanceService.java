b28f2aed75791e5e47cbd4ca143fac4deff7d4aa
jinyoung jang <jyjang@uengine.org>
2017-08-23 13:55:40 +0900

    InstanceService and Instance Monitoring - https://github.com/TheOpenCloudEngine/process-codi-mw4/issues/11

--- src/main/java/org/uengine/social/service/InstanceService.java
+++ src/main/java/org/uengine/social/service/InstanceService.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 import org.uengine.kernel.*;
-import org.uengine.modeling.resource.*;
+import org.uengine.modeling.resource.ResourceManager;
 import org.uengine.social.entity.WorklistEntity;
 import org.uengine.social.repository.WorklistRepository;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
 
 /**
  * Created by uengine on 2017. 8. 9..
@@ -20,81 +21,23 @@ import javax.annotation.PostConstruct;
  *  - json must be Typed JSON to enable object polymorphism - need to change the jackson engine. TODO: accept? typed json is sometimes hard to read
  */
 @RestController
-public class WorkItemService {
+public class InstanceService {
 
-    @Autowired
-    ResourceManager resourceManager;
+    @RequestMapping(value = "/instance/{instanceId}/variables", method = RequestMethod.GET)
+    public Map getProcessVariables(@PathVariable("instanceId") String instanceId) throws Exception {
 
-    @Autowired
-    DefinitionService definitionService;
-
-    @Autowired
-    WorklistRepository worklistRepository;
-
-    @PostConstruct
-    public void init(){
-    }
-
-    // ---------------- CRUD mappings -------------------- //
-
-    @RequestMapping(value = "/work-item/{taskId}", method = RequestMethod.GET)
-    public WorkItem getWorkItem(@PathVariable("taskId") String taskId) throws Exception {
-
-        WorklistEntity worklistEntity = worklistRepository.findOne(new Long(taskId));
-
-        if(worklistEntity==null)
-            throw new Exception("No such work item where taskId = " + taskId);
-
-        String defId = worklistEntity.getDefId();
-
-        ProcessDefinition definition = (ProcessDefinition) definitionService.getDefinitionLocal(defId);
-        Activity activity = definition.getActivity(worklistEntity.getTrcTag());
-
-        WorkItem workItem = new WorkItem();
-        workItem.setActivity(activity);
-        workItem.setWorklist(worklistEntity);
-
-        return workItem;
-    }
-
-    @RequestMapping(value = "/work-item/{taskId}", method = RequestMethod.POST)
-    public void putWorkItem(@PathVariable("taskId") String definitionPath, @RequestBody WorkItem workItem) throws Exception {
-        Long taskId = workItem.getWorklist().getTaskId();
-
-        Long instId = workItem.getWorklist().getInstId();
-
-        ProcessDefinition definition = (ProcessDefinition) definitionService.getDefinitionLocal(workItem.getWorklist().getDefId());
-        org.uengine.kernel.ProcessInstance instance = applicationContext.getBean(
-                org.uengine.kernel.ProcessInstance.class,
+        ProcessInstance instance = applicationContext.getBean(
+                ProcessInstance.class,
                 new Object[]{
-                        definition,
-                        instId.toString(),
+                        null,
+                        instanceId,
                         null
                 }
         );
 
-        HumanActivity humanActivity = ((HumanActivity)instance.getProcessDefinition().getActivity(workItem.getWorklist().getTrcTag()));
 
-        if(!instance.isRunning(humanActivity.getTracingTag()) && !humanActivity.isNotificationWorkitem()){
-            throw new UEngineException("Illegal completion for workitem [" + humanActivity + ":"+ humanActivity.getStatus(instance) +"]: Already closed or illegal status.");
-        }
-
-
-        if("saved".equals(workItem.getDesiredState())){
-            humanActivity.saveWorkItem(instance, null);
-        }else{
-            try{
-                humanActivity.fireReceived(instance, null);
-            }catch(Exception e){
-                humanActivity.fireFault(instance, e);
-
-                throw new UEngineException(e.getMessage(), null, new UEngineException(e.getMessage(), e), instance, humanActivity);
-            }
-        }
-
-
+        return ((DefaultProcessInstance)instance).getVariables();
     }
-
 
     @Autowired
     ApplicationContext applicationContext;
