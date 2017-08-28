e6b3ffce4cb9cc969de7d53bcb206da283143beb
SeungpilPark <sppark@uengine.org>
2017-08-28 11:04:32 +0900

    #13 launch script

--- launch.sh
+++ launch.sh
@@ -3,6 +3,9 @@
 # try in deployment server
 
 pkill -f java
+
 pkill -f node
-java -jar target/uengine-five-portal-1.0.0-SNAPSHOT.jar
-http-server src/main/resources/dist
+
+nohup mvn spring-boot:run &
+
+nohup http-server src/main/resources/dist -p 8081 &
