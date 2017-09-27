8a8ae314dab43764070212c7ce376a637006ff91
jinyoung jang <jyjang@uengine.org>
2017-09-27 12:59:33 +0900

    Adding a new Task type - CallActivity

---
+++ src/main/resources/dev/src/components/bpmn/task/CallActivity.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <geometry-element
+      selectable
+      movable
+      resizable
+      connectable
+      deletable
+      :id.sync="activity.tracingTag"
+      :x.sync="activity.elementView.x"
+      :y.sync="activity.elementView.y"
+      :width.sync="activity.elementView.width"
+      :height.sync="activity.elementView.height"
+      :_style.sync="style"
+      :parentId.sync="activity.elementView.parent"
+      :label.sync="activity.name.text"
+      v-on:dblclick="showProperty"
+      v-on:selectShape="closeComponentChanger"
+      v-on:deSelectShape="closeComponentChanger"
+      v-on:removeShape="closeComponentChanger"
+      v-on:redrawShape="closeComponentChanger"
+    >
+      <geometry-rect
+        :_style="{
+          'fill-r': 1,
+          'fill-cx': .1,
+          'fill-cy': .1,
+          'stroke-width': 3,
+          fill: '#FFFFFF',
+          'fill-opacity': 0,
+          r: '10'
+        }"
+      >
+      </geometry-rect>
+
+      <sub-elements>
+
+        <bpmn-loop-type :loopType="loopType"></bpmn-loop-type>
+        <bpmn-state-animation :status="status" :type="type"></bpmn-state-animation>
+      </sub-elements>
+      <bpmn-sub-controller :type="type"></bpmn-sub-controller>
+    </geometry-element>
+
+    <bpmn-property-panel
+      :drawer.sync="drawer"
+      :item.sync="activity"
+    >
+      <template slot="properties-contents">
+        <md-input-container>
+          <label>액티비티 명</label>
+          <md-input type="text"
+                    v-model="activity.name.text"></md-input>
+        </md-input-container>
+        <md-input-container>
+          <label>연결 프로세스 정의</label>
+
+          <md-select name="movie" id="movie" v-model="activity.definitionId">
+            <md-option value="fight_club">Fight Club</md-option>
+            <md-option value="godfather">Godfather</md-option>
+            <md-option value="godfather_ii">Godfather II</md-option>
+            <md-option value="godfather_iii">Godfather III</md-option>
+            <md-option value="godfellas">Godfellas</md-option>
+            <md-option value="pulp_fiction">Pulp Fiction</md-option>
+            <md-option value="scarface">Scarface</md-option>
+          </md-select>
+
+        </md-input-container>
+        <md-input-container>
+          <label>retryDelay</label>
+          <md-input type="number"
+                    v-model.number="activity.retryDelay"></md-input>
+        </md-input-container>
+      </template>
+      <template slot="additional-tabs">
+
+      </template>
+    </bpmn-property-panel>
+  </div>
+</template>
+
+<script>
+  import IBpmn from '../IBpmn'
+  export default {
+    mixins: [IBpmn],
+    name: 'bpmn-call-activity',
+    props: {},
+    computed: {
+      defaultStyle(){
+        return {}
+      },
+      type(){
+        return 'Task'
+      },
+      className(){
+        return 'org.uengine.kernel.bpmn.CallActivity'
+      }
+    },
+    data: function () {
+      return {};
+    },
+    watch: {},
+    mounted: function () {
+
+    },
+    methods: {}
+  }
+</script>
+
+
+<style scoped lang="scss" rel="stylesheet/scss">
+
+</style>
+
