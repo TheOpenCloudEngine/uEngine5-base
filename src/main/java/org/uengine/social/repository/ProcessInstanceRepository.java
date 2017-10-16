1149f8c9d5fed7ba9a25f12860662d5227a70d6d
Yongmin Jo <lusky87@gmail.com>
2017-10-16 15:58:08 +0900

    #18 Instance Filter BackEnd

--- src/main/java/org/uengine/social/repository/ProcessInstanceRepository.java
+++ src/main/java/org/uengine/social/repository/ProcessInstanceRepository.java
@@ -2,6 +2,7 @@ package org.uengine.social.repository;
 
 import org.metaworks.multitenancy.persistence.MultitenantRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.uengine.social.entity.ProcessInstanceEntity;
 
@@ -16,5 +17,15 @@ public interface ProcessInstanceRepository extends MultitenantRepository<Process
     @Query("select pi from ProcessInstanceEntity pi where exists (select 1 from WorklistEntity wl where wl.endpoint = ?#{loggedUserId})")
     List<ProcessInstanceEntity> findAllICanSee();
 
+    @Query("select pi from ProcessInstanceEntity pi where pi.defId like CONCAT('%',:defId,'%') or pi.instId = :instId or pi.status = :status or pi.eventHandler = :eventHandler or pi.name like :name or pi.startedDate = :startedDate or pi.finishedDate = :finishedDate")
+    List<ProcessInstanceEntity> findFilterICanSee(
+                                                  @Param("defId") String defId,
+                                                  @Param("instId") Long instId,
+                                                  @Param("status") String status,
+                                                  @Param("eventHandler") String eventHandler,
+                                                  @Param("name") String name,
+                                                  @Param("startedDate") String startedDate,
+                                                  @Param("finishedDate") String finishedDate
+                                                  );
 }
 
--- src/main/resources/dev/src/components/InstanceList.vue
+++ src/main/resources/dev/src/components/InstanceList.vue
@@ -4,17 +4,103 @@
       <md-layout md-flex="15">
         <div class="phone-viewport">
           <md-toolbar md-theme="white">
-            <span class="md-title">프로세스 목록</span>
+            <span class="md-title">프로세스</span>
           </md-toolbar>
 
           <md-list>
             <md-list-item md-expand-multiple>
               <md-icon>folder</md-icon>
-              <span class="md-body-1">LEVEL1</span>
+              <span class="md-body-1">프로세스 목록</span>
               <md-list-expand>
                 <md-list>
                   <md-list-item class="md-inset" v-for="tree in trees" :key="tree.name">
                     {{tree.name}}
+                  </md-list-item>
+                </md-list>
+              </md-list-expand>
+            </md-list-item>
+          </md-list>
+          <md-list>
+            <md-list-item md-expand-multiple>
+              <md-icon>folder</md-icon>
+              <span class="md-body-1">인스턴스 검색</span>
+              <md-list-expand>
+                <md-list>
+                  <md-list-item class="md-inset">
+                    <md-input-container>
+                      <md-select name="status" id="status" v-model="filter.status" @change="setStatus">
+                        <md-option value="all">모두</md-option>
+                        <md-option value="doing">진행중</md-option>
+                        <md-option value="ready">준비중</md-option>
+                        <md-option value="complete">완료됨</md-option>
+                        <md-option value="stop">중지됨</md-option>
+                        <md-option value="skip">건너뜀</md-option>
+                        <md-option value="pause">일시중지</md-option>
+                        <md-option value="fail">실패함</md-option>
+                      </md-select>
+                    </md-input-container>
+                  </md-list-item>
+                  <md-list-item class="md-inset">
+                    <md-input-container>
+                      <label>인스턴스 아이디</label>
+                      <md-input placeholder="Instance ID" v-model="filter.instId"></md-input>
+                    </md-input-container>
+                  </md-list-item>
+                  <md-list-item class="md-inset">
+                    <md-input-container>
+                      <label>이름</label>
+                      <md-input placeholder="name" v-model="filter.name"></md-input>
+                    </md-input-container>
+                  </md-list-item>
+                  <md-list-item class="md-inset">
+                    <md-input-container>
+                      <label>시작자</label>
+                      <md-input placeholder="starter" v-model="filter.defName"></md-input>
+                    </md-input-container>
+                    <md-dialog-prompt
+                      :md-title="prompt.title"
+                      :md-ok-text="prompt.ok"
+                      :md-cancel-text="prompt.cancel"
+                      @open="onOpen"
+                      @close="onClose"
+                      ref="dialog6">
+                    </md-dialog-prompt>
+
+                    <md-button class="md-icon-button md-raised md-primary" @click.native="openDialog('dialog6')">
+                      <md-icon style="color: white">search</md-icon>
+                    </md-button>
+                  </md-list-item>
+                  <md-list-item class="md-inset">
+                    <md-input-container>
+                      <label>현담당자</label>
+                      <md-input placeholder="current manager" v-model="filter.eventHandler"></md-input>
+                    </md-input-container>
+                    <md-dialog-prompt
+                      :md-title="prompt.title"
+                      :md-ok-text="prompt.ok"
+                      :md-cancel-text="prompt.cancel"
+                      @open="onOpen"
+                      @close="onClose"
+                      ref="dialog6">
+                    </md-dialog-prompt>
+                    <md-button class="md-icon-button md-raised md-primary">
+                      <md-icon style="color: white">search</md-icon>
+                    </md-button>
+                  </md-list-item>
+                  <md-list-item class="md-inset">
+                    <md-input-container>
+                      <label>시작일</label>
+                      <md-input placeholder="Start Date" v-model="filter.startedDate"></md-input>
+                    </md-input-container>
+                  </md-list-item>
+                  <md-list-item class="md-inset">
+                    <md-input-container>
+                      <label>종료일</label>
+                      <md-input placeholder="End Date" v-model="filter.finishedDate"></md-input>
+                    </md-input-container>
+                  </md-list-item>
+                  <md-list-item class="md-inset">
+                    <md-button class="md-raised md-primary" v-on:click="search()">Search</md-button>
                   </md-list-item>
                 </md-list>
               </md-list-expand>
@@ -51,9 +137,11 @@
   </md-layout>
 </template>
 <script>
+
   export default {
-    data () {
+    data() {
       return {
+        status: 'all',
         headers: [
           {text: '상태', value: 'status'},
           {text: '아이디', value: 'instId'},
@@ -67,6 +155,16 @@
           {text: 'Ext1', value: 'ext1'},
           {text: '삭제', value: 'instId'}
         ],
+        prompt: {
+          title: 'What\'s your name?',
+          ok: 'Done',
+          cancel: 'Cancel',
+          id: 'name',
+          name: 'name',
+          placeholder: 'Type your name...',
+          maxlength: 30,
+          value: ''
+        },
         items: [
           {
             instId: 'instId',
@@ -86,7 +184,20 @@
           {
             name: 'name'
           }
-        ]
+        ],
+        filter:{
+          instId: 'instId',
+          defName: 'defName',
+          defId: 'defId',
+          name: 'name',
+          status: 'status',
+          eventHandler: 'eventHandler',
+          isSubProcess: 'isSubProcess',
+          startedDate: 'startedDate',
+          info: 'info',
+          ext1: 'ext1',
+          finishedDate: 'finishedDate'
+        }
       }
     },
     mounted() {
@@ -124,6 +235,32 @@
         this.$router.push({
           path: 'instance/' + instId
         })
+      },
+      search: function () {
+        console.log(this.filter.instId);
+        var url = 'instances/search/findFilterICanSee?';
+        if (this.filter.instId != null ){
+          url += 'instId=' + this.filter.instId;
+        }
+        this.$router.push({
+
+          path: url
+        })
+      },
+      setStatus: function (status) {
+        this.status = status;
+      },
+      openDialog(ref) {
+        this.$refs[ref].open();
+      },
+      closeDialog(ref) {
+        this.$refs[ref].close();
+      },
+      onOpen() {
+        console.log('Opened');
+      },
+      onClose(type) {
+        console.log('Closed', type);
       }
     }
   }
