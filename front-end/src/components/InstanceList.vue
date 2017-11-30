00776189aeea9be2960b156cc82b6bc89960dfa0
jo-narae <nare6004@gmail.com>
2017-11-30 12:06:55 +0900

    instance list

--- front-end/src/components/InstanceList.vue
+++ front-end/src/components/InstanceList.vue
@@ -96,7 +96,7 @@
         </md-list>
       </md-layout>
       <md-layout md-flex-xsmall="100" md-flex-small="80" md-flex-medium="80" md-flex-large="80">
-        <md-table-card class="instance-select">
+        <md-table-card>
           <md-table @select="onSelect">
             <md-table-header>
               <md-table-row>
@@ -104,7 +104,7 @@
               </md-table-row>
             </md-table-header>
             <md-table-body v-if="items.length > 0">
-              <md-table-row v-for="item in items" :md-item="item" md-auto-select md-selection style="cursor:pointer">
+              <md-table-row v-for="item in items" :md-item="item" md-auto-select md-selection style="cursor:pointer" @click.native="onClickList(item.instId)">
                 <md-table-cell>{{item.status}}</md-table-cell>
                 <md-table-cell>{{item.instId}}</md-table-cell>
                 <md-table-cell>{{item.defId}}</md-table-cell>
@@ -283,9 +283,12 @@
         console.log(this.filter);
       },
       onSelect: function (item) {
-        //selected instance list move page
+        //selected instance list
+      },
+      onClickList: function (instId) {
+        //move to instance detail page
         this.$router.push({
-          path: 'instance/' + item[0].instId
+          path: 'instance/' + instId
         })
       },
       onPagination(e) {
@@ -304,7 +307,4 @@
     margin-top: 50px;
   }
   td { text-align: center; }
-  .instance-select .md-table-selection {
-    display:none;
-  }
 </style>
