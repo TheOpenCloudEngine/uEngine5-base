04210b285de6660932e17108b48e42f03ebc578f
Kim-youngtak <bigkoe@gmail.com>
2017-12-20 11:32:01 +0900

    Variable Service 구현 - UI에서 우선은  String 형으로만 받아서 설정되도록 변경, 추후 각 객체타입별로 처리해야 하는 부분 필요
    https://github.com/TheOpenCloudEngine/uEngine5-base/issues/50

---
+++ process-service/src/main/java/org/uengine/five/service/VariableService.java
@@ -0,0 +1,45 @@
+package org.uengine.five.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.*;
+import org.uengine.five.framework.ProcessTransactional;
+import org.uengine.kernel.*;
+
+import java.io.Serializable;
+
+/**
+ * Created by uengine on 2017. 8. 9..
+ *
+ * Implementation Principles:
+ *  - REST Maturity Level : 2
+ *  - Not using old uEngine ProcessManagerBean, this replaces the ProcessManagerBean
+ *  - ResourceManager and CachedResourceManager will be used for definition caching (Not to use the old DefinitionFactory)
+ *  - json must be Typed JSON to enable object polymorphism - need to change the jackson engine. TODO: accept? typed json is sometimes hard to read
+ */
+@RestController
+public class VariableService {
+
+    // ---------------- CRUD mappings -------------------- //
+
+    @Autowired
+    InstanceServiceImpl instanceService;
+
+    @RequestMapping(value = "/instance/{instId}/variable/{varName}", method = RequestMethod.GET)
+    @ProcessTransactional(readOnly = true)
+    public Serializable getVariable(@PathVariable("instId") String instId, @PathVariable("varName") String varName) throws Exception {
+        ProcessInstance instance = instanceService.getProcessInstanceLocal(instId);
+        return instance.get("",varName);
+    }
+
+    @RequestMapping(value = "/instance/{instanceId}/variable/{varName}", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
+    @ProcessTransactional
+    public void setVariable(@PathVariable("instanceId") String instanceId, @PathVariable("varName") String varName, @RequestParam("varValue") String varValue) throws Exception {
+        ProcessInstance instance = instanceService.getProcessInstanceLocal(instanceId);
+        instance.set("",varName, (Serializable) varValue);
+    }
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+}
