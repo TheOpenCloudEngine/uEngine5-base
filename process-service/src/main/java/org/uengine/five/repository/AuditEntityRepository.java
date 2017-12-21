2e24ebfeb5f91e3ec92077df2ffc73b2d5919085
jinyoung jang <jyjang@uengine.org>
2017-12-21 12:37:50 +0900

    Audit Table 및 Compensation
    https://github.com/TheOpenCloudEngine/uEngine5-base/issues/55

---
+++ process-service/src/main/java/org/uengine/five/repository/AuditEntityRepository.java
@@ -0,0 +1,14 @@
+package org.uengine.five.repository;
+
+import org.metaworks.multitenancy.persistence.MultitenantRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.uengine.five.entity.AuditEntity;
+import org.uengine.five.entity.ProcessInstanceEntity;
+
+/**
+ * Created by uengine on 2017. 12. 21..
+ */
+
+@RepositoryRestResource(collectionResourceRel = "audits", path = "audit")
+public interface AuditEntityRepository extends MultitenantRepository<AuditEntity, Long> {
+}
